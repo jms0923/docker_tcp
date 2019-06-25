3b771b38ed620139e76c7ed2d763f709b96aa9e1
jms <jms588@naver.com>
2019-06-25 18:06:14 +0900

    json obj

--- threadServer.py
+++ threadServer.py
@@ -10,7 +10,8 @@ def recv_msg(client_socket, addr):
     jsonObj = makeJsonObject(message)
     # print(type(jsonObj))
     # print(jsonObj)
-    print(str(addr) + ' : ' + jsonObj)
+    print(type(jsonObj))
+    print(str(addr) + ' : ', jsonObj)
     disconnect(client_socket)
     print(str(addr) + ' ' + 'disconnected\n')
 
